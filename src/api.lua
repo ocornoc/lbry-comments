9721d3656734df61294c3ef493bd8b3a51ee4a23
ocornoc <ocornoc@protonmail.com>
2018-12-15 10:58:29 -0800

    Added vote undo and reply-getting

--- src/api.lua
+++ src/api.lua
@@ -33,7 +33,7 @@ local json = require "cjson"
 local api = {}
 local error_code = {}
 
-local api_VERSION = "0.5.0"
+local api_VERSION = "0.6.0"
 
 --------------------------------------------------------------------------------
 -- Helpers
@@ -204,22 +204,35 @@ end
 -- `params.uri`: A string containing a full-length permanent LBRY claim URI.
 -- If the URI isn't valid/acceptable, the function will return with an
 -- `error_code.INVALID_URI` response.
+--
+-- `params.undo`: An optional boolean containing whether the upvote is being
+-- undone. If this is true, then rather than giving a vote, a vote is being
+-- retracted. Defaults to `false`.
 -- @treturn int The new total amount of upvotes.
 -- @usage {"jsonrpc": "2.0", "method": "upvote_claim", "id": 1, "params": {
 -- 	"uri": "lbry://lolkris#53ecfd214b62f38b1bec9849b7a69127b30cd26c"
 -- }} -> [server]
 -- [server] -> {"jsonrpc": "2.0", "id": 1, "result": 5}
+-- @usage {"jsonrpc": "2.0", "method": "upvote_claim", "id": 1, "params": {
+-- 	"uri": "lbry://lolkris#53ecfd214b62f38b1bec9849b7a69127b30cd26c",
+-- 	"undo": true,
+-- }} -> [server]
+-- [server] -> {"jsonrpc": "2.0", "id": 1, "result": 4}
 function api.upvote_claim(params)
 	if type(params.uri) ~= "string" then
 		return nil, make_error"'uri' must be a string"
 	elseif not valid_perm_uri(params.uri) then
 		return nil, make_error("'uri' unacceptable form",
 		                       error_code.INVALID_URI)
+	elseif params.undo == nil or params.undo == json.null then
+		params.undo = false
+	elseif type(params.undo) ~= "boolean" then
+		return nil, make_error"'undo' must be bool/null/omitted"
 	end
 	
 	-- We get the data for the claim to tell if it exists. If it doesn't
 	-- exist in the database, we create it on-demand.
-	local _, err_msg = db.claims.get_data(params.uri)
+	local data, err_msg = db.claims.get_data(params.uri)
 	
 	if err_msg == "uri doesnt exist" then
 		local success, err_msg = db.claims.new(params.uri)
@@ -235,9 +248,21 @@ function api.upvote_claim(params)
 		end
 	elseif err_msg then
 		return nil, make_error(err_msg, error_code.INTERNAL)
+	-- If someone is trying to unupvote a claim that has 0 upvotes,
+	-- something is definitely not right.
+	elseif data.upvotes == 0 and params.undo then
+		ngx.log(ngx.ALERT, "uri: " .. params.uri .. "tried to " ..
+		        "unuvt. a claim with 0 uvt.s")
+		
+		return nil, make_error"cant unupvote 0 upvotes"
 	end
 	
-	local total, err_msg = db.claims.upvote(params.uri)
+	-- If undo, send -1 to db.claims.upvote.
+	-- Otherwise, send 1.
+	local total, err_msg = db.claims.upvote(
+	                        params.uri,
+				(params.undo and -1) or 1
+	                       )
 	
 	if total and not err_msg then
 		return total
@@ -256,22 +281,35 @@ end
 -- `params.uri`: A string containing a full-length permanent LBRY claim URI.
 -- If the URI isn't valid/acceptable, the function will return with an
 -- `error_code.INVALID_URI` response.
+--
+-- `params.undo`: An optional boolean containing whether the downvote is being
+-- undone. If this is true, then rather than giving a vote, a vote is being
+-- retracted. Defaults to `false`.
 -- @treturn int The new total amount of downvotes.
 -- @usage {"jsonrpc": "2.0", "method": "downvote_claim", "id": 1, "params": {
 -- 	"uri": "lbry://lolkris#53ecfd214b62f38b1bec9849b7a69127b30cd26c"
 -- }} -> [server]
 -- [server] -> {"jsonrpc": "2.0", "id": 1, "result": 5}
+-- @usage {"jsonrpc": "2.0", "method": "downvote_claim", "id": 1, "params": {
+-- 	"uri": "lbry://lolkris#53ecfd214b62f38b1bec9849b7a69127b30cd26c",
+-- 	"undo": true,
+-- }} -> [server]
+-- [server] -> {"jsonrpc": "2.0", "id": 1, "result": 4}
 function api.downvote_claim(params)
 	if type(params.uri) ~= "string" then
 		return nil, make_error"'uri' must be a string"
 	elseif not valid_perm_uri(params.uri) then
 		return nil, make_error("'uri' unacceptable form",
 		                       error_code.INVALID_URI)
+	elseif params.undo == nil or params.undo == json.null then
+		params.undo = false
+	elseif type(params.undo) ~= "boolean" then
+		return nil, make_error"'undo' must be bool/null/omitted"
 	end
 	
 	-- We get the data for the claim to tell if it exists. If it doesn't
 	-- exist in the database, we create it on-demand.
-	local _, err_msg = db.claims.get_data(params.uri)
+	local data, err_msg = db.claims.get_data(params.uri)
 	
 	if err_msg == "uri doesnt exist" then
 		local success, err_msg = db.claims.new(params.uri)
@@ -287,9 +325,21 @@ function api.downvote_claim(params)
 		end
 	elseif err_msg then
 		return nil, make_error(err_msg, error_code.INTERNAL)
+	-- If someone is trying to undownvote a claim that has 0 downvotes,
+	-- something is definitely not right.
+	elseif data.upvotes == 0 and params.undo then
+		ngx.log(ngx.ALERT, "uri: " .. params.uri .. "tried to " ..
+		        "undvt. a claim with 0 dvt.s")
+		
+		return nil, make_error"cant unupvote 0 upvotes"
 	end
 	
-	local total, err_msg = db.claims.downvote(params.uri)
+	-- If undo, send -1 to db.claims.downvote.
+	-- Otherwise, send 1.
+	local total, err_msg = db.claims.downvote(
+	                        params.uri,
+				(params.undo and -1) or 1
+	                       )
 	
 	if total and not err_msg then
 		return total
@@ -601,28 +651,51 @@ end
 --
 -- `params.comm_index`: An int containing the ID of the comment that is going to
 -- be upvoted.
+--
+-- `params.undo`: An optional boolean containing whether the upvote is being
+-- undone. If this is true, then rather than giving a vote, a vote is being
+-- retracted. Defaults to `false`.
 -- @treturn[1] int The new total amount of upvotes.
 -- @treturn[2] NULL There is no comment with the given ID.
 -- @usage {"jsonrpc": "2.0", "method": "upvote_comment", "id": 1, "params": {
 -- 	"comment_id": 1
 -- }} -> [server]
 -- [server] -> {"jsonrpc": "2.0", "id": 1, "result": 1}
+-- @usage {"jsonrpc": "2.0", "method": "upvote_comment", "id": 1, "params": {
+-- 	"comment_id": 1,
+-- 	"undo": true,
+-- }} -> [server]
+-- [server] -> {"jsonrpc": "2.0", "id": 1, "result": 0}
 function api.upvote_comment(params)
 	if type(params.comm_index) ~= "number" then
 		return nil, make_error"'comm_index' must be an int"
 	elseif params.comm_index % 1 ~= 0 then
 		return nil, make_error"'comm_index' must be an int"
+	elseif params.undo == nil or params.undo == json.null then
+		params.undo = false
+	elseif type(params.undo) ~= "boolean" then
+		return nil, make_error"'undo' must be bool/null/omitted"
 	end
 	
-	-- We get the data for the claim to tell if it exists. If it doesn't
-	-- exist in the database, we create it on-demand.
-	local _, err_msg = db.comments.get_data(params.comm_index)
+	local data, err_msg = db.comments.get_data(params.comm_index)
 	
+	-- If someone is trying to unupvote a comment that has 0 upvotes,
+	-- something is definitely not right.
 	if err_msg then
 		return nil, make_error(err_msg)
+	elseif data.upvotes == 0 and params.undo then
+		ngx.log(ngx.ALERT, "id: " .. params.comm_index .. "tried to " ..
+		        "unuvt. a comment with 0 uvt.s")
+		
+		return nil, make_error"cant unupvote 0 upvotes"
 	end
 	
-	local total, err_msg = db.comments.upvote(params.comm_index)
+	-- If undo, send -1 to db.comments.upvote.
+	-- Otherwise, send 1.
+	local total, err_msg = db.comments.upvote(
+	                        params.comm_index,
+				(params.undo and -1) or 1
+	                       )
 	
 	if total and not err_msg then
 		return total
@@ -640,28 +713,51 @@ end
 --
 -- `params.comm_index`: An int containing the ID of the comment that is going to
 -- be downvoted.
+--
+-- `params.undo`: An optional boolean containing whether the downvote is being
+-- undone. If this is true, then rather than giving a vote, a vote is being
+-- retracted. Defaults to `false`.
 -- @treturn[1] int The new total amount of downvotes.
 -- @treturn[2] NULL There is no comment with the given ID.
 -- @usage {"jsonrpc": "2.0", "method": "downvote_comment", "id": 1, "params": {
 -- 	"comment_id": 1
 -- }} -> [server]
 -- [server] -> {"jsonrpc": "2.0", "id": 1, "result": 1}
+-- @usage {"jsonrpc": "2.0", "method": "downvote_comment", "id": 1, "params": {
+-- 	"comment_id": 1,
+-- 	"undo": true,
+-- }} -> [server]
+-- [server] -> {"jsonrpc": "2.0", "id": 1, "result": 0}
 function api.downvote_comment(params)
 	if type(params.comm_index) ~= "number" then
 		return nil, make_error"'comm_index' must be an int"
 	elseif params.comm_index % 1 ~= 0 then
 		return nil, make_error"'comm_index' must be an int"
+	elseif params.undo == nil or params.undo == json.null then
+		params.undo = false
+	elseif type(params.undo) ~= "boolean" then
+		return nil, make_error"'undo' must be bool/null/omitted"
 	end
 	
-	-- We get the data for the claim to tell if it exists. If it doesn't
-	-- exist in the database, we create it on-demand.
-	local _, err_msg = db.comments.get_data(params.comm_index)
+	local data, err_msg = db.comments.get_data(params.comm_index)
 	
+	-- If someone is trying to undownvote a comment that has 0 downvotes,
+	-- something is definitely not right.
 	if err_msg then
 		return nil, make_error(err_msg)
+	elseif data.downvotes == 0 and params.undo then
+		ngx.log(ngx.ALERT, "id: " .. params.comm_index .. "tried to " ..
+		        "undvt. a comment with 0 dvt.s")
+		
+		return nil, make_error"cant undownvote 0 downvotes"
 	end
 	
-	local total, err_msg = db.comments.downvote(params.comm_index)
+	-- If undo, send -1 to db.comments.downvote.
+	-- Otherwise, send 1.
+	local total, err_msg = db.comments.downvote(
+	                        params.comm_index,
+				(params.undo and -1) or 1
+	                       )
 	
 	if total and not err_msg then
 		return total
@@ -671,6 +767,60 @@ function api.downvote_comment(params)
 		ngx.log(ngx.ERR, "weird error [downvote_comment]: (" .. total ..
 		        ", " .. err_msg .. ")")
 		return nil, make_error("unknown", error_code.UNKNOWN)
+	end
+end
+
+--- Returns all direct replies to a comment.
+-- @tparam table params The table of parameters.
+--
+-- `params.comm_index`: An int containing the ID of the comment that is going to
+-- be downvoted.
+-- @treturn[1] table An array of replies.
+--
+-- Fields for each comment:
+--
+-- `comm_index`: An int holding the index of the comment.
+--
+-- `claim_index`: An int holding the index of the claims that this is a
+-- comment on.
+--
+-- `poster_name`: A string holding the name of the poster.
+--
+-- `parent_com`: An int holding the `comment_index` field of another comment
+-- object that is the parent of this comment. Because these comments are always
+-- top-level comments, the field is omitted (`nil`).
+--
+-- `post_time`: An int representing the time of the row's insertion into the
+-- database, stored as UTC Epoch seconds.
+--
+-- `message`: A string holding the body of the comment.
+--
+-- `upvotes`: An int representing the amount of upvotes for that comment.
+--
+-- `downvotes`: An int representing the amount of downvotes for that
+-- comment.
+--
+-- @treturn[2] NULL There is no comment with the given ID.
+-- @usage {"jsonrpc": "2.0", "method": "get_comment_replies", "id": 1,
+--  "params": {
+-- 	"comm_index": 20
+-- }} -> [server]
+-- [server] -> {"jsonrpc": "2.0", "id": 1, "result": [...]}
+function api.get_comment_replies(params)
+	if type(params.comm_index) ~= "number" then
+		return nil, make_error"'comm_index' must be an int"
+	elseif params.comm_index % 1 ~= 0 then
+		return nil, make_error"'comm_index' must be an int"
+	end
+	
+	local replies, err_msg = db.comments.get_replies(params.comm_index)
+	
+	if err_msg == "comment doesnt exist" then
+		return json.null
+	elseif err_msg then
+		return nil, make_error(err_msg, error_code.INTERNAL)
+	else
+		return replies
 	end
 end
 
