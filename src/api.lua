4e32129f1e1be35e3668cf83744c1cefa93efd99
Oleg Silkin <o.silkin98@gmail.com>
2019-01-03 17:32:04 +0000

    Added require to api.lua
    Closes issue #17

--- src/api.lua
+++ src/api.lua
@@ -28,6 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 --------------------------------------------------------------------------------
 
+local crypto = require "crypto"
 local db = require "db"
 local json = require "cjson"
 local api = {}
